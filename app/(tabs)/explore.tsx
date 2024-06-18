a095e28516e072125cfc41ac43a8c95dbe4b4eee
Adam Hari <adam.hari@kashoo.com>
2024-06-18 17:39:38 -0600

    feat: make headerImage boring

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -90,7 +90,7 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
+    color: '#999',
     bottom: -90,
     left: -35,
     position: 'absolute',
